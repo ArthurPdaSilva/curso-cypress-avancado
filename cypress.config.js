ceeeea867d15453c3cc9b0f487b3c4d25fe319e4
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2025-08-25 13:54:51 -0300

    feat: trocando o linter

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://wlsf82-hacker-stories.web.app',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    baseUrl: 'https://wlsf82-hacker-stories.web.app'
+  }
+})
