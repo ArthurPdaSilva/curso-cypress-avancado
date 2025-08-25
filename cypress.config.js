ad8a5d4d65e777b8b255e7acced02cec639d55fb
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2025-08-25 13:41:08 -0300

    feat: atualizando projeto

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://wlsf82-hacker-stories.web.app',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
