6b8a9b51c44a45a600baaded1c7026e288c43f4c
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2025-08-25 15:11:32 -0300

    Update hackerStories.spec.cy.js

--- cypress/e2e/hackerStories.spec.cy.js
+++ cypress/e2e/hackerStories.spec.cy.js
@@ -1,4 +1,7 @@
 describe("Hacker Stories", () => {
+	const initialTerm = "React";
+	const newTerm = "Cypress";
+
 	const interceptSearch = (query = "React", page = "0") => {
 		cy.intercept({
 			method: "GET",
@@ -10,28 +13,12 @@ describe("Hacker Stories", () => {
 		}).as("search");
 	};
 
-	beforeEach(() => {
-		interceptSearch();
-
-		cy.visit("/");
-
-		cy.wait("@search", { timeout: 2000 });
-	});
-
-	it("shows the footer", () => {
-		cy.get("footer")
-			.should("be.visible")
-			.and("contain", "Icons made by Freepik from www.flaticon.com");
-	});
-
-	context("List of stories", () => {
-		// Since the API is external,
-		// I can't control what it will provide to the frontend,
-		// and so, how can I assert on the data?
-		// This is why this test is being skipped.
-		// TODO: Find a way to test it out.
-		// it.skip pula o teste
-		it.skip("shows the right data for all rendered stories", () => {});
+	context("Hitting the real API", () => {
+		beforeEach(() => {
+			interceptSearch();
+			cy.visit("/");
+			cy.wait("@search", { timeout: 2000 });
+		});
 
 		it('shows 20 stories, then the next 20 after clicking "More"', () => {
 			interceptSearch("React", "1");
@@ -44,6 +31,37 @@ describe("Hacker Stories", () => {
 			cy.get(".item").should("have.length", 40);
 		});
 
+		it("searches via the last searched term", () => {
+			interceptSearch("Cypress", "0");
+			cy.get("#search").clear().type(`${newTerm}{enter}`);
+
+			cy.wait("@search", { timeout: 2000 });
+
+			cy.get(`button:contains(${initialTerm})`).should("be.visible").click();
+
+			cy.wait("@search", { timeout: 2000 });
+
+			cy.get(".item").should("have.length", 20);
+			cy.get(".item").first().should("contain", initialTerm);
+			cy.get(`button:contains(${newTerm})`).should("be.visible");
+		});
+	});
+
+	beforeEach(() => {
+		interceptSearch();
+		cy.visit("/");
+		cy.wait("@search", { timeout: 2000 });
+	});
+
+	it("shows the footer", () => {
+		cy.get("footer")
+			.should("be.visible")
+			.and("contain", "Icons made by Freepik from www.flaticon.com");
+	});
+
+	context("List of stories", () => {
+		it.skip("shows the right data for all rendered stories", () => {});
+
 		// it.only("shows only nineteen stories after dimissing the first story", () => {...) O only roda só esse teste e ignora os demais
 		it("shows only nineteen stories after dimissing the first story", () => {
 			cy.get(".button-small").first().click();
@@ -51,11 +69,6 @@ describe("Hacker Stories", () => {
 			cy.get(".item").should("have.length", 19);
 		});
 
-		// Since the API is external,
-		// I can't control what it will provide to the frontend,
-		// and so, how can I test ordering?
-		// This is why these tests are being skipped.
-		// TODO: Find a way to test them out.
 		context.skip("Order by", () => {
 			it("orders by title", () => {});
 
@@ -65,21 +78,9 @@ describe("Hacker Stories", () => {
 
 			it("orders by points", () => {});
 		});
-
-		// Hrm, how would I simulate such errors?
-		// Since I still don't know, the tests are being skipped.
-		// TODO: Find a way to test them out.
-		context.skip("Errors", () => {
-			it('shows "Something went wrong ..." in case of a server error', () => {});
-
-			it('shows "Something went wrong ..." in case of a network error', () => {});
-		});
 	});
 
 	context("Search", () => {
-		const initialTerm = "React";
-		const newTerm = "Cypress";
-
 		beforeEach(() => {
 			cy.get("#search").clear();
 			interceptSearch(newTerm, "0");
@@ -106,31 +107,13 @@ describe("Hacker Stories", () => {
 			cy.get(`button:contains(${initialTerm})`).should("be.visible");
 		});
 
-    // É um fluxo só para mostrar que é possível, porém o teste a seguir não é um fluxo que o usuário faria ou seria capaz de fazer
+		// É um fluxo só para mostrar que é possível, porém o teste a seguir não é um fluxo que o usuário faria ou seria capaz de fazer
 		it("types and submits the form directly", () => {
-			cy.get("#search")
-				.should("be.visible")
-				.clear()
-				.type(newTerm);
+			cy.get("#search").should("be.visible").clear().type(newTerm);
 			cy.get(".search-form").submit();
 		});
 
 		context("Last searches", () => {
-			it("searches via the last searched term", () => {
-				interceptSearch("Cypress", "0");
-				cy.get("#search").type(`${newTerm}{enter}`);
-
-				cy.wait("@search", { timeout: 2000 });
-
-				cy.get(`button:contains(${initialTerm})`).should("be.visible").click();
-
-				cy.wait("@search", { timeout: 2000 });
-
-				cy.get(".item").should("have.length", 20);
-				cy.get(".item").first().should("contain", initialTerm);
-				cy.get(`button:contains(${newTerm})`).should("be.visible");
-			});
-
 			it("shows a max of 5 buttons for the last searched terms", () => {
 				const { faker } = require("@faker-js/faker");
 
@@ -143,6 +126,42 @@ describe("Hacker Stories", () => {
 
 				cy.get(".last-searches button").should("have.length", 5);
 			});
+		});
+	});
+
+	context("Error handling", () => {
+		const interceptServerError = (errorType) => {
+			cy.intercept(
+				{
+					method: "GET",
+					pathname: "**/search",
+					query: {
+						query: "React",
+						page: "0",
+					},
+				},
+				{
+					forceNetworkError: errorType === "network",
+					statusCode: errorType === "server" ? 500 : undefined,
+				},
+			).as("search");
+		};
+
+		const visitHomePageAndCheckError = () => {
+			cy.visit("/");
+			cy.wait("@search", { timeout: 2000 });
+
+			cy.get("p:contains('Something went wrong ...')").should("be.visible");
+		};
+
+		it('shows "Something went wrong ..." in case of a server error', () => {
+			interceptServerError("server");
+			visitHomePageAndCheckError();
+		});
+
+		it('shows "Something went wrong ..." in case of a network error', () => {
+			interceptServerError("network");
+			visitHomePageAndCheckError();
 		});
 	});
 });
